ed713d727a26c68743169c3bcf721cfda01a50a4
unknown <nechetov@hotmail.com>
2020-12-28 13:58:42 +0300

    edit feed and router

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,9 +1,48 @@
 <template>
-  <div>Feed {{ apiUrl }}</div>
+  <div>
+    <div v-if="isLoading">Loading...</div>
+    <div v-if="error">Something bad happed</div>
+
+    <div v-if="feed">
+      <div class="article-preview" v-for="(article, index) in feed.articles" :key="index">
+        <div class="article-meta">
+          <router-link
+            :to="{ name: 'userProfile', params: { slug: article.author.username } }"
+          >
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: { slug: article.author.username },
+              }"
+            >
+              {{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">ADD TO FAVORITES</div>
+        </div>
+        <router-link
+          :to="{ name: 'article', params: { slug: article.slug } }"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          TAG LIST
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
+  </div>
 </template>
 
 <script>
-import {actionTypes} from '@/store/modules/feed';
+import { mapState } from 'vuex';
+
+import { actionTypes } from '@/store/modules/feed';
 
 export default {
   name: 'McvFeed',
@@ -13,8 +52,15 @@ export default {
       required: true,
     },
   },
+  computed: {
+    ...mapState({
+      isLoading: (state) => state.feed.isLoading,
+      feed: (state) => state.feed.data,
+      error: (state) => state.feed.error,
+    }),
+  },
   mounted() {
-    this.$store.dispatch(actionTypes.getFeed, {apiUrl: this.apiUrl});
+    this.$store.dispatch(actionTypes.getFeed, { apiUrl: this.apiUrl });
   },
 };
 </script>
